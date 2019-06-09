dcba04264c626e9ffda62b23223938143465910b
Igor Martins <igorm573@gmail.com>
2019-06-09 10:05:39 -0300

    service worker was corriged.

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
-var CACHE_NAME = 'db';
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  'index.html',
+  'css/bootstrap.min.css',
+  'css/style.css',
+  'js/manifest.js',
+  'js/bootstrap.min.js',
+  'js/popper.min.js'
+];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll([
-        'index.html',
-        'css/style.css',
-        'css/bootstrap.min.css',
-        'js/manifest.js',
-        'js/jquery-3.3.1.slim.min.js',
-        'js/popper.min.js',
-        'js/bootstrap.min.js',
-        'icons/img1.png'
-      ]);
-    })
-  )
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('activate', function activator(event) {
@@ -22,9 +25,11 @@ self.addEventListener('activate', function activator(event) {
     caches.keys().then(function (keys) {
       return Promise.all(keys
         .filter(function (key) {
+          console.log("oi")
           return key.indexOf(CACHE_NAME) !== 0;
         })
         .map(function (key) {
+          console.log("oi2")
           return caches.delete(key);
         })
       );
@@ -32,10 +37,28 @@ self.addEventListener('activate', function activator(event) {
   );
 });
 
-self.addEventListener('fetch', function (event) {
+addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function (cachedResponse) {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  return cache.match('index.html');
+                });
+            });
+        }
+      })
   );
-});
+}); 
