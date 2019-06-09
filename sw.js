acee563865b18a2acbfa679d404384244b057a53
Igor Martins <igorm573@gmail.com>
2019-06-09 14:18:20 -0300

    facial recog was implemented

--- sw.js
+++ sw.js
@@ -6,12 +6,10 @@ var urlsToCache = [
   'js/manifest.js',
   'js/bootstrap.min.js',
   'js/popper.min.js',
-  'js/tracking.js',
-  'js/eye.js',
-  'js/face.js',
-  'js/mouth.js',
   'https://igormarti.github.io/pwa_facial_recognition/',
-  'https://igormarti.github.io'
+  'js/app.js',
+  'js/tracking-min.js',
+  'js/face-min.js',
 ];
 
 self.addEventListener('install', function(event) {
